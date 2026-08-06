f5d299844f31e5c4abdfe77151e3c6af77a18f8a
Fermat2358 <gerrydoch@gmail.com>
2026-08-06 15:47:04 +0300

    Lesson 4 Gradient and Guidance added

--- scripts/build_site.py
+++ scripts/build_site.py
@@ -64,6 +64,8 @@ SOURCES = {
         "03_Intermediate/01_Lessons/Straight_Lines/Lesson_02_A_Table_Can_Predict_a_Line/NEO_Maths_Y10_Straight_Lines_Lesson_02_A_Table_Can_Predict_a_Line_v0.1.html",
     "straight-lines-03-where-does-the-line-start":
         "03_Intermediate/01_Lessons/Straight_Lines/Lesson_03_Where_Does_the_Line_Start/NEO_Maths_Y10_Straight_Lines_Lesson_03_Where_Does_the_Line_Start_v0.1.html",
+    "straight-lines-04-how-fast-is-the-line-changing":
+        "03_Intermediate/01_Lessons/Straight_Lines/Lesson_04_How_Fast_Is_the_Line_Changing/NEO_Maths_Y10_Straight_Lines_Lesson_04_How_Fast_Is_the_Line_Changing_v0.1.html",
     "ks3-ratio-and-proportion":
         "KS3 (1)/ratio (1)/NEO_Maths_KS3_Ratio_Proportion_Interactive_v6.html",
     "ks3-place-value":
