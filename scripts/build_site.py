811a178f0a08b1da69663cdf91d1b6b9bbb02f6a
Fermat2358 <gerrydoch@gmail.com>
2026-08-10 14:50:49 +0300

    Lesson 10 Consolidation and Guidance Added

--- scripts/build_site.py
+++ scripts/build_site.py
@@ -76,6 +76,8 @@ SOURCES = {
         "03_Intermediate/01_Lessons/Straight_Lines/Lesson_08_When_Two_Lines_Meet/NEO_Maths_Y10_Straight_Lines_Lesson_08_When_Two_Lines_Meet_v0.1.html",
     "straight-lines-09-conversion-graphs-as-models":
         "03_Intermediate/01_Lessons/Straight_Lines/Lesson_09_Conversion_Graphs/NEO_Maths_Y10_Straight_Lines_Lesson_09_Conversion_Graphs_v0.1.html",
+    "straight-lines-10-what-can-i-explain":
+        "03_Intermediate/01_Lessons/Straight_Lines/Lesson_10_What_Can_I_Explain/NEO_Maths_Y10_Straight_Lines_Lesson_10_What_Can_I_Explain_v0.1.html",
     "ks3-ratio-and-proportion":
         "KS3 (1)/ratio (1)/NEO_Maths_KS3_Ratio_Proportion_Interactive_v6.html",
     "ks3-place-value":
