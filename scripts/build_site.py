5929f193d1cee0986ce4ee2b16bef05e4876b016
Fermat2358 <gerrydoch@gmail.com>
2026-08-16 14:54:39 +0300

    Trigonometry Lesson 2 and Guidance added

--- scripts/build_site.py
+++ scripts/build_site.py
@@ -80,6 +80,8 @@ SOURCES = {
         "03_Intermediate/01_Lessons/Straight_Lines/Lesson_10_What_Can_I_Explain/NEO_Maths_Y10_Straight_Lines_Lesson_10_What_Can_I_Explain_v0.1.html",
     "trigonometry-01-the-constant-ratio":
         "03_Intermediate/01_Lessons/Introduction_to_Trigonometry/Lesson_01_The_Constant_Ratio/NEO_Maths_Y10_Trigonometry_Lesson_01_The_Constant_Ratio_v0.1.html",
+    "trigonometry-02-naming-the-sides":
+        "03_Intermediate/01_Lessons/Introduction_to_Trigonometry/Lesson_02_Naming_the_Sides/NEO_Maths_Y10_Trigonometry_Lesson_02_Naming_the_Sides_v0.1.html",
     "ks3-ratio-and-proportion":
         "KS3 (1)/ratio (1)/NEO_Maths_KS3_Ratio_Proportion_Interactive_v6.html",
     "ks3-place-value":
