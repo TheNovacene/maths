4b77d37f3c6c2b07b2eec00a410171672fa3e096
Fermat2358 <gerrydoch@gmail.com>
2026-09-24 15:15:45 +0300

    Circle Theorems Lesson 7 added with guidance.

--- scripts/build_site.py
+++ scripts/build_site.py
@@ -142,6 +142,8 @@ SOURCES = {
         "03_Intermediate/01_Lessons/Circle_Theorems/Lesson_05_Cyclic_Quadrilaterals/NEO_Maths_Y10_Circle_Theorems_Lesson_05_Cyclic_Quadrilaterals_v0.1.html",
     "circle-theorems-06-tangents":
         "03_Intermediate/01_Lessons/Circle_Theorems/Lesson_06_Tangents/NEO_Maths_Y10_Circle_Theorems_Lesson_06_Tangents_v0.1.html",
+    "circle-theorems-07-the-alternate-segment-theorem":
+        "03_Intermediate/01_Lessons/Circle_Theorems/Lesson_07_The_Alternate_Segment_Theorem/NEO_Maths_Y10_Circle_Theorems_Lesson_07_The_Alternate_Segment_Theorem_v0.1.html",
     "trigonometry-01-the-constant-ratio":
         "03_Intermediate/01_Lessons/Introduction_to_Trigonometry/Lesson_01_The_Constant_Ratio/NEO_Maths_Y10_Trigonometry_Lesson_01_The_Constant_Ratio_v0.1.html",
     "trigonometry-02-naming-the-sides":
